6fa7eaa915ddc0036a1f83f588a1cfe1de4992dc
andres_jaramillo <ajaramillo@bancofalabella.com.co>
2020-09-13 10:22:07 -0500

    delete redundant

--- task_manager/views.py
+++ task_manager/views.py
@@ -3,24 +3,10 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from task_manager.models import Task
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from task_manager.serializer import TaskSerializer, PaginatorTaskSerializer, UpdateTaskSerializer
-
-
-class AuthTokenResource(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key
-        }, status=status.HTTP_200_OK)
 
 
 class TaskResource(APIView):
